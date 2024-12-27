2a13aa46d6a77ec81bbfa2f5b8e20a8d08bc9c68
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2024-12-27 23:56:11 +0530

    feat: add route for product detail page

--- frontend/src/routes/vendorRoute.jsx
+++ frontend/src/routes/vendorRoute.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import DetailProduct from '../features/vendor/pages/DetailProduct';
 
 import VendorDashboard from '../features/vendor/pages/VendorDashboard';
 import Profile from '../features/vendor/pages/Profile';
@@ -11,6 +12,7 @@ const VendorRoutes = () => {
       <Route path="/vendor-dashboard" element={<VendorDashboard />} />
       <Route path="/vendor/profile" element={<Profile />} />
       <Route path="/products" element={<Products />} />
+      <Route path="/products/:id" element={<DetailProduct />} />
     </Routes>
   );
 };
