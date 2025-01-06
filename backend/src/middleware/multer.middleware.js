53b6c5d6d915aa6117d477a1e9796f588aae6dc6
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-01-06 22:56:18 +0530

    .

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,7 +1,7 @@
 import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
-//FIXME : not working
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const uploadPath = path.join(
