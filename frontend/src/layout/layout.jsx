3fb3e84caa15d0d4712896edf55d26c0bbc0cf21
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-01-10 23:50:34 +0530

    chore: enhance layout

--- frontend/src/layout/layout.jsx
+++ frontend/src/layout/layout.jsx
@@ -2,24 +2,41 @@ import { Box } from '@mui/material';
 import Navbar from '../components/Navbar';
 import Footer from '../components/footer';
 import BreadCrumbs from '../components/BreadCrumb';
+import EcoFriendlyNavbar from '../components/Dropdown';
+import { useSelector } from 'react-redux';
 const Layout = ({ children }) => {
+  const { user } = useSelector((state) => state.auth);
+
+  const role = user?.user?.role || user?.data?.user?.role;
+
   return (
     <Box
       sx={{
         display: 'flex',
         flexDirection: 'column',
         minHeight: '100vh',
+        width: '100%',
         backgroundColor: '#ffff',
       }}
     >
-      <Navbar />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 6,
+        }}
+      >
+        <Navbar />
+        {role === 'user' || null ? <EcoFriendlyNavbar /> : <></>}
+      </Box>
 
       <Box
         component="main"
         sx={{
           display: 'flex',
           flexDirection: 'column',
-          py: 5,
+          my: 1,
+          px: 1,
         }}
       >
         <BreadCrumbs />
