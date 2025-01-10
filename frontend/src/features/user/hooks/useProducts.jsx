8daeb8588a8e0128125cb1a485c7855f2e6174ae
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-01-10 23:45:51 +0530

    feat: add custom hook for product fetching by category

---
+++ frontend/src/features/user/hooks/useProducts.jsx
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { getProductByCategory } from '../../product/productSlice.js';
+import { getProductsThunk } from '../../product/productSlice.js';
+
+export const useProducts = () => {
+  const dispatch = useDispatch();
+  const { products, status } = useSelector((state) => state.product);
+
+  const [category, setCategory] = useState(null);
+
+  const fetchProductsByCategory = (category) => {
+    dispatch(getProductByCategory(category));
+  };
+
+  useEffect(() => {
+    if (category) {
+      fetchProductsByCategory(category);
+    } else {
+      dispatch(getProductsThunk());
+    }
+  }, [category, dispatch]);
+
+  return { products, status, setCategory };
+};
