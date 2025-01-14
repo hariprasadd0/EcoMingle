699b3d9470181cf5cd3792377cdd6b7f5a24f2c7
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-01-14 21:46:03 +0530

    .

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,10 +4,6 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    'Access-Control-Allow-Credentials': true,
   },
   withCredentials: true,
 });
